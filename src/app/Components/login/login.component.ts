e2ba6281871e3a406a79296db67e5dff3ec6f9f4
19130026 <107161736+19130026@users.noreply.github.com>
2023-08-07 21:51:16 +0700

    login
    sử lí sự kiện login

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {ApiService} from "../../service/api.service";
 import {Router} from "@angular/router";
 import {FormBuilder, FormGroup} from "@angular/forms";
-
+import {Product} from "./product";
 import {CartService} from "../../service/cart.service";
 
 
@@ -12,12 +12,58 @@ import {CartService} from "../../service/cart.service";
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
+  public cartItems : any =[];
+  constructor(private formBuilder:FormBuilder,private apiService:ApiService,private router:Router) { }
+  public err: string = "";
+  public loginForm!:FormGroup;
   ngOnInit(): void {
-
+    this.loginForm = this.formBuilder.group({
+      email:[''],
+      password:['']
+    })
 
   }
 
+  login(){
+    this.apiService.getCustomers().subscribe(
+      (res) => {
+        const user = res.find((a: any)=>{
+          return (a.user_email === this.loginForm.value.email || a.user_name === this.loginForm.value.email ) &&
+            a.user_password === this.loginForm.value.password
+        })
+        // localStorage.setItem("cart",JSON.stringify(this.cartItems));
+        localStorage.setItem('user',JSON.stringify(user));
+        if(user){
 
+
+          if(user.role == "") {
+
+            this.router.navigate(["products"]);
+          }
+            // else if(user.role == "admin") {
+            //   alert("Login Successful!!!");
+            //   this.router.navigate(["adminHome"]);
+            // }
+            // else if(user.role == "vendor") {
+            //   alert("Login Successful!!!");
+            //   this.router.navigate(["home"]); // dòng này dùnd de chuyen trang
+          // }
+          else{
+            this.err = "Thông tin đăng nhập sai";
+          }
+          this.loginForm.reset();
+
+        }
+        else{
+          this.err = "Nhập sai tài khoản hoặc mật khẩu";
+
+        }
+
+      },err=>{
+        this.err = "Đã xảy ra lỗi !!!";
+
+      }
+    )
+  }
 
 }
--- src/app/Components/login/product.ts
+++ src/app/Components/login/product.ts
@@ -1,1 +1,10 @@
+export class Product {
+  id: String | undefined
+  name: String | undefined
+  user_name: String | undefined
+  user_email: String | undefined
+  user_password: String | undefined
+  user_address: String | undefined
+  role: String | undefined
+}
 
