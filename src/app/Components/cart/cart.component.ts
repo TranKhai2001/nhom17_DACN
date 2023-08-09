5046a9584a00e8741c236ddec75f99a225cdd2d7
NgTrKhanhHuy <91460118+NgTrKhanhHuy@users.noreply.github.com>
2023-08-09 12:52:21 +0700

    chức năng giỏ hàng

--- src/app/Components/cart/cart.component.ts
+++ src/app/Components/cart/cart.component.ts
@@ -11,9 +11,47 @@ import {CartService} from "../../service/cart.service";
 })
 export class CartComponent implements OnInit {
 
+  public products : any = [];
+  constructor(private router:Router,private http:HttpClient,private apiService:ApiService,private cartService : CartService) { }
 
   ngOnInit(): void {
-
+    this.cartService.getProducts()
+      .subscribe(res=>{
+        this.products = res;
+      })
+  }
+  removeItem(item: any){
+    this.cartService.removeCartItem(item);
+  }
+  emptycart(){
+    this.cartService.removeAllCart();
   }
 
+  getTotalPriceEeachProduct(product: any){
+    return product.rate * product.num;
+  }
+
+  getFeeShip(){
+    return this.cartService.feeShip();
+  }
+
+  getTotalMoneyCart(){
+    return this.cartService.getTotalPrice();
+  }
+
+  getTotalFinalFee(){
+    return this.cartService.getTotalPrice() + this.getFeeShip();
+  }
+
+  // removeProduct(product) {
+  //   this.productRemoved.emit(product)
+  // }
+
+
+  modelChanged(product: any) {
+    if (product.num === 0) {
+      this.cartService.removeCartItem(product)
+    }
+
+  }
 }
