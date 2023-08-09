a1568990195fb0ab85291291c1c79a7521e34aad
khai <19130094@st.hcmuaf.edu.vn>
2023-08-09 10:50:57 +0700

    xử lý chức ăng comment

--- src/app/Components/comment/comment.component.ts
+++ src/app/Components/comment/comment.component.ts
@@ -11,9 +11,34 @@ import {FormBuilder, FormControl, FormGroup, Validator, Validators} from "@angul
   styleUrls: ['./comment.component.scss']
 })
 export class CommentComponent implements OnInit {
+  comments: Array<Comment>=[];
+  page1: number=1;
+  totalComment: number=0;
+  // @ts-ignore
+  form!:FormGroup
+  @Input() initialText:string='';
+  @Input() submitLabel:string='';
+  @Input() currentUserId:string='';
+  @Output() handleSubmit =new EventEmitter<string>();
+  constructor( private activatedRoute: ActivatedRoute,private router:Router,private http:HttpClient,private apiService:ApiService,private fb:FormBuilder) { }
 
   ngOnInit(): void {
-
+    this.apiService.getComment().subscribe(comments=>{
+      // @ts-ignore
+      this.comments=comments;
+      this.totalComment=this.comments.length;
+      console.log('comments',comments);
+    })
+    this.form=this.fb.group({
+      title:new FormControl(this.initialText,Validators.required)
+    })
+  }
+  onSubmit(){
+    console.log(this.form.value);
+    this.handleSubmit.emit(this.form.value.title)
   }
 
+  refresh() {
+    window.location.reload();
+  }
 }
