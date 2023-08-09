a0823afb392202c23bb5c369abc715eb2513ae6d
19130026 <107161736+19130026@users.noreply.github.com>
2023-08-09 14:22:18 +0700

    thông tin tai khoản
    cập nhật giao diện thông tin tài khoản

--- src/app/Components/user-profile/user-profile.component.ts
+++ src/app/Components/user-profile/user-profile.component.ts
@@ -6,12 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-profile.component.scss']
 })
 export class UserProfileComponent implements OnInit {
+  hoten: String="";
+  sdt: String="";
+  email: String="";
+  gioitinh: String="";
+  ngaysinh: String="";
+  diachi: String="";
 
+  constructor() { }
 
   ngOnInit(): void {
-
+    this.infor()
   }
 
+  infor(){
+    var curr = localStorage.getItem('user');
+    const text = localStorage.getItem('user');
+    const obj = JSON.parse(text+"");
+    this.hoten = obj.name;
+    this.sdt = obj.phone;
+    this.diachi = obj.user_address;
+    this.gioitinh = obj.sex;
+    this.email = obj.user_email;
+    this.ngaysinh = obj.user_birth;
 
+  }
 
 }
