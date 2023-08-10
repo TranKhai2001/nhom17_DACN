750a24409e8fefe2b5cbe6f68a451c55dc084035
khai <19130094@st.hcmuaf.edu.vn>
2023-08-10 12:02:18 +0700

    Xử lý chức năng tìm kiếm , thoát và hiển thị tên người dùng khi đăng nhập

--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -5,7 +5,7 @@ import {CartService} from "../../service/cart.service";
 import {ApiService} from "../../service/api.service";
 import {ActivatedRoute, Router} from "@angular/router";
 import {HttpClient} from "@angular/common/http";
-
+import {Product} from "../login/product";
 
 
 
@@ -16,14 +16,45 @@ import {HttpClient} from "@angular/common/http";
 })
 export class HeaderComponent implements OnInit {
 
+  public totalItem : number = 0;
+  public user: any;
+
+
+
+  constructor(private activatedRoute: ActivatedRoute,private router:Router,private http:HttpClient,private apiService:ApiService,private cartService : CartService) { }
 
 
   ngOnInit(): void {
-
+    this.cartService.getProducts()
+      .subscribe(res=>{
+        this.totalItem = res.length;
+      })
 
 
   }
 
+  getUserCurrent(){
+    var curr = localStorage.getItem('user');
+    const text = localStorage.getItem('user');
+    const obj = JSON.parse(text+"");
+    return obj.name;
 
+  }
+
+
+  logOut() {
+    localStorage.removeItem('user');
+    this.router.navigate(["login"]);
+    // this.cartService.removeAllCart();
+  }
+
+  loggined() {
+    return localStorage.getItem('user');
+  }
+
+
+  totalproductIncart(){
+    return this.cartService.calcTotal();
+  }
 
 }
